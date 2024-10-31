e7d4ace24acf7e71668cfec20b4f65a8ddd5ff21
IT21185298 <it21185298@my.sliit.lk>
2024-10-31 15:54:59 +0530

    add profile icon

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -12,8 +12,32 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black26,
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.black26,
+        title: const Text(
+          'Profile',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.zero, // Removes padding around the image
+                  shape: CircleBorder(),
+                  fixedSize: Size(60, 60)),
+              child: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/profile_picture.jpg',
+                ), // or NetworkImage for a URL
+                radius: 20, // Adjust the size of the profile picture
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
